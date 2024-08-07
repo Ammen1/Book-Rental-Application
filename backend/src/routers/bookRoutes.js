1741a7da3d561339ddde11cf9a854171df8d8b56
Tamirat <amenguda@gmail.com>
2024-08-07 05:34:10 +0300

    Set up routes for book management and statistics- Added route for creating a book with validation and authorization- Added route for listing books with authorization- Added route for fetching a book by ID with authorization- Added route for fetching books by owner ID with authorization- Added route for updating a book with validation and authorization- Added route for deleting a book with authorization- Added route for fetching book statistics with authorization

---
+++ backend/src/routers/bookRoutes.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import { createBook, listBooks, getBookById, getBooksByOwnerId, updateBook, deleteBook, getStatistics } from '../controllers/bookController.js';
+import { validateBook } from '../validation/validateBook.js';
+import { checkAbilities } from '../middlewares/checkAbility.js';
+import { isAuthenticated } from '../middlewares/auth.js';
+
+
+const router = express.Router();
+
+router.use(isAuthenticated);
+
+router.post(
+  '/',
+  validateBook,
+  checkAbilities('create', 'Book'),
+  createBook
+);
+
+router.get(
+  '/',
+  checkAbilities('read', 'Book'),
+  listBooks
+);
+router.get(
+  '/:id',
+  checkAbilities('read', 'Book'),
+  getBookById
+);
+
+router.get(
+  '/owner/:ownerId',
+  checkAbilities('read', 'Book'),
+  getBooksByOwnerId
+);
+
+router.put(
+  '/:id',
+  validateBook,
+  checkAbilities('update', 'Book'),
+  updateBook
+);
+
+router.delete(
+  '/:id',
+  checkAbilities('delete', 'Book'),
+  deleteBook
+);
+router.get('/data/statistics',checkAbilities('read', 'Book'), getStatistics);
+
+export default router;
