f608bc8d5ee8c6f6bdf061de8a6828536b278842
Tamirat <amenguda@gmail.com>
2024-08-20 15:33:55 +0300

    Update Vercel configuration for static builds and routing; adjust Vite build output

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-
-
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:3000",
-//         secure: false,
-//       },
-//     },
-//   },
-//   plugins: [react()],
-// });
+  build: {
+    outDir: 'dist', // Ensure this matches the output directory Vercel expects
+  },
+});
